3fb859467ef943ba6a1e8e22723e4eee77c0de96
Abbafei <mail+git@abbafei.com>
2020-04-29 15:53:29 -0400

    bH schema checking

--- _data/items.js
+++ _data/items.js
@@ -2,5 +2,5 @@
 const proc_items = require("../source/proc_items");
 
 module.exports = async function() {
-    return await proc_items({items_file_path: "source/items.yml", types_file_path: "source/types.yml"});
+    return await proc_items({items_file_path: "source/items.yml", types_file_path: "source/types.yml", items_schema_file_path: "source/items.schema.json"});
 };
--- source/proc_items.js
+++ source/proc_items.js
@@ -5,13 +5,28 @@ const path = require("path");
 
 const jq = require("node-jq");
 const js_yaml = require("js-yaml");
+const ajv = require("ajv");
 
-async function proc_items({items_file_path="items.yml", types_file_path="types.yml"}={}) {
+function validate_schema({schema, data}={}) {
+    const ajv_obj = ajv();
+    const validate = ajv_obj.compile(schema);
+    const valid = validate(data);
+    return {ajv: ajv_obj, valid, validate};
+}
+
+async function proc_items({items_file_path="items.yml", types_file_path="types.yml", items_schema_file_path}={}) {
     const proc_items_code = await util.promisify(fs.readFile)(path.join(__dirname, "proc_items_code.jq"), "utf8");
     const items_inp_str = JSON.stringify(js_yaml.safeLoad(await util.promisify(fs.readFile)(items_file_path)));
     const types_inp_str = JSON.stringify(js_yaml.safeLoad(await util.promisify(fs.readFile)(types_file_path)));
+    const items_schema = JSON.parse(await util.promisify(fs.readFile)(items_schema_file_path));
     const proced_items_str = await jq.run(proc_items_code, [items_inp_str, types_inp_str].join(""), {input: "string", output: "compact", slurp: true});
-    return JSON.parse(proced_items_str);
+    const out_json = JSON.parse(proced_items_str);
+    const items_validation_result = validate_schema({schema: items_schema, data: out_json["dict"]});
+    if (!items_validation_result.valid) {
+        throw new Error(`items.json validation error(s): ${JSON.stringify(items_validation_result.validate.errors, null, 2)}`);
+        //console.error(items_validation_result.validate.errors);
+    };
+    return out_json;
 }
 
 if (require.main === module) {
