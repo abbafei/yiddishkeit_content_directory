c5415c1e0f6b36f091585a41d6948075fb602beb
Abbafei <mail+git@abbafei.com>
2020-05-11 23:31:28 -0400

    bH add blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,23 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const pluginNavigation = require("@11ty/eleventy-navigation");
+const pluginFeed = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    // plugins
+    eleventyConfig.addPlugin(pluginNavigation);
+    eleventyConfig.addPlugin(pluginFeed);
+
+    // processing
     eleventyConfig.addWatchTarget("./source/items.yml");
     eleventyConfig.addWatchTarget("./source/types.yml");
+    eleventyConfig.setFrontMatterParsingOptions({
+        excerpt: true
+    });
+
+    // content
+    eleventyConfig.addCollection("blog", function(collection) {
+        return collection.getFilteredByGlob("blog/post/*");
+    });
+    eleventyConfig.addFilter("as_post_date", function(date_obj) {
+        return date_obj.toLocaleDateString("default", {calendar: "hebrew", timeZone: "utc", weekday: "long", year: "2-digit", month: "numeric", day: "numeric"});
+    });
 };
---
+++ blog/post/welcome.md
@@ -0,0 +1,9 @@
+---
+title: Welcome!
+date: 2020-05-11
+layout: post
+---
+Welcome to Yiddishkeit.info!
+---
+
+B'ezras Hashem (with G-d's help), updates will be posted here from time to time.
